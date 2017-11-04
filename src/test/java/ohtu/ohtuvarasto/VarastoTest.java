04d45e881a1ccef0397dc22fc595ec7cd67c9ecd
Julius Laitala <jiilaitala@gmail.com>
2017-11-04 13:58:32 +0200

    Made lisaysLisaaSaldoa fail

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -66,7 +66,7 @@ public class VarastoTest {
 
     @Test
     public void lisaysLisaaSaldoa() {
-        varasto.lisaaVarastoon(8);
+        varasto.lisaaVarastoon(3);
 
         // saldon pitäisi olla sama kun lisätty määrä
         assertEquals(8, varasto.getSaldo(), vertailuTarkkuus);
